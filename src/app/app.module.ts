e0013c23fedaa4426a2487f2aaf25f642d982cb0
NielDevSft <85716027+NielDevSft@users.noreply.github.com>
2024-03-16 15:52:23 -0300

    checkpoint

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {}
